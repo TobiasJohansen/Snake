8da9b6c614c8ace76f0824c5466276c0e00ea11c
Tobias Johansen <tobjoh1994@gmail.com>
2017-04-29 15:33:31 +0200

    Commenting mainwindow.cpp

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,20 +6,26 @@
 #include <QTimer>
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent){
+
+    //Window init
     QGraphicsScene* scene = new QGraphicsScene(this);
     QGraphicsView* view = new QGraphicsView(scene, this);
     setFixedSize(610, 610);
     setCentralWidget(view);
 
+    //Walls
     QGraphicsRectItem* wall = new QGraphicsRectItem();
     wall->setRect(0, 0, 600, 600);
     scene->addItem(wall);
+
+    //"Snake"
     MyRect* item = new MyRect();
     item->setRect(0, 0, 30, 30);
     item->setFlag(QGraphicsItem::ItemIsFocusable);
     item->setFocus();
     scene->addItem(item);
 
+    //Timer which runs the logic 4 times each second
     QTimer* timer = new QTimer();
     connect(timer, SIGNAL(timeout()), item, SLOT(move()));
     timer->start(250);
